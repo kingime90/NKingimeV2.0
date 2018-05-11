8cdbcc926b7dbf712a42a49b2f3d1187de0cb9e4
kingime90 <kingime90@163.com>
2018-05-11 21:24:35 +0800

    代码整合

--- app/NKingime.App.Mvc/App_Start/BundleConfig.cs
+++ app/NKingime.App.Mvc/App_Start/BundleConfig.cs
@@ -9,23 +9,23 @@ namespace NKingime.App.Mvc
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/static/shared/js/jquery-{version}.js"));
 
             // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
             // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/static/shared/js/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib/bootstrap").Include(
+                      "~/static/lib/bootstrap/js/bootstrap.js",
+                      "~/static/lib/bootstrap/js/bootstrap-table.js",
+                      "~/static/lib/bootstrap/js/bootstrap-table-zh-CN.js",
+                      "~/static/shared/js/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/bundles/css/bootstrap").Include(
+                      "~/static/lib/bootstrap/css/bootstrap.css",
+                      "~/static/lib/bootstrap/css/bootstrap-table.css",
+                      "~/static/shared/css/site.css"));
         }
     }
 }
