41c36be58cce6444e33014cf3c7bb51a49c54923
kingime90 <kingime90@163.com>
2018-05-21 22:55:50 +0800

    DTO

---
+++ src/NKingime.Core/Dto/EntityDtoProfile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AutoMapper;
+
+namespace NKingime.Core.Dto
+{
+    /// <summary>
+    /// 实体DTO自定义映射配置。 
+    /// </summary>
+    /// <typeparam name="TEntityDto">实体DTO类型。</typeparam>
+    /// <typeparam name="TEntity">实体类型。</typeparam>
+    public abstract class EntityDtoProfile<TEntityDto, TEntity> : Profile where TEntityDto : class, IEntityDto
+    {
+        /// <summary>
+        /// 初始化一个<see cref="EntityDtoProfile{TEntityDto,TEntity}"/>成功实例。
+        /// </summary>
+        public EntityDtoProfile()
+        {
+            var expression = CreateMap<TEntityDto, TEntity>();
+            Configure(expression);
+        }
+
+        /// <summary>
+        /// 配置。
+        /// </summary>
+        /// <param name="expression"></param>
+        public virtual void Configure(IMappingExpression<TEntityDto, TEntity> expression)
+        {
+
+        }
+    }
+}
---
+++ src/NKingime.Core/Dto/IEntityDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NKingime.Core.Dto
+{
+    /// <summary>
+    /// 定义实体DTO接口。
+    /// </summary>
+    public interface IEntityDto
+    {
+
+    }
+}
