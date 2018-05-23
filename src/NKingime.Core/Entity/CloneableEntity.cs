d5e9ddfa08e4a9e793b352089483a39127df6196
kingime90 <kingime90@163.com>
2018-05-23 23:42:35 +0800

    对象深复制

--- src/NKingime.Core/Entity/CloneableEntity.cs
+++ src/NKingime.Core/Entity/CloneableEntity.cs
@@ -1,34 +1,20 @@
 ﻿using System;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using NKingime.Utility.General;
 
 namespace NKingime.Core.Entity
 {
     /// <summary>
     /// 可复制的数据实体基类。
     /// </summary>
-    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TKey">主键类型。</typeparam>
     [Serializable]
-    public abstract class CloneableEntity<TKey> : IEntity<TKey> where TKey : IEquatable<TKey>
+    public abstract class CloneableEntity<TKey> : Cloneable, IEntity<TKey> where TKey : IEquatable<TKey>
     {
         /// <summary>
         /// 主键ID。
         /// </summary>
         public abstract TKey Id { get; set; }
-
-        /// <summary>
-        /// 创建作为当前实例副本的新对象。
-        /// </summary>
-        /// <returns></returns>
-        public object Clone()
-        {
-            var formatter = new BinaryFormatter();
-            var stream = new MemoryStream();
-            formatter.Serialize(stream, this);
-            stream.Seek(0, SeekOrigin.Begin);
-            object result = formatter.Deserialize(stream);
-            stream.Close();
-            return result;
-        }
     }
 }
---
+++ src/NKingime.Utility/General/Cloneable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace NKingime.Utility.General
+{
+    /// <summary>
+    /// 支持克隆，即用与现有实例相同的值创建类的新实例。
+    /// </summary>
+    [Serializable]
+    public class Cloneable : ICloneable
+    {
+        /// <summary>
+        /// 创建作为当前实例副本的新对象。
+        /// </summary>
+        /// <returns></returns>
+        public object Clone()
+        {
+            var formatter = new BinaryFormatter();
+            var stream = new MemoryStream();
+            formatter.Serialize(stream, this);
+            stream.Seek(0, SeekOrigin.Begin);
+            object result = formatter.Deserialize(stream);
+            stream.Close();
+            return result;
+        }
+    }
+}
