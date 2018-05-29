28e405967157a37384986afde079dbcc873e85d5
kingime90 <kingime90@163.com>
2018-05-29 22:26:52 +0800

    AutoMapper扩展方法

--- src/NKingime.Core/Extensions/AutoMapperExtensions.cs
+++ src/NKingime.Core/Extensions/AutoMapperExtensions.cs
@@ -22,12 +22,11 @@ namespace NKingime.Core.Extensions
         /// <summary>
         /// 映射到指定的目标类型。
         /// </summary>
-        /// <typeparam name="TSource">source 类型。</typeparam>
         /// <typeparam name="TTarget">指定的目标类型。</typeparam>
-        /// <param name="source">source 类型实例。</param>
+        /// <param name="source">实体DTO接口实例。</param>
         /// <param name="target">指定的目标类型的实例。</param>
         /// <returns></returns>
-        public static TTarget MapTo<TSource, TTarget>(this TSource source, TTarget target) where TSource : class, IEntityDto where TTarget : class
+        public static TTarget MapTo<TTarget>(this IEntityDto source, TTarget target) where TTarget : class
         {
             return AutoMapper.Mapper.Map(source, target);
         }
